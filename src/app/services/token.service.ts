100534a7f1711137c63100b484f584580e93e495
CristianRM05 <crm00423@gmail.com>
2025-01-24 09:56:36 +0100

    cambios para que el token se borre a las 24 horas

--- src/app/services/token.service.ts
+++ src/app/services/token.service.ts
@@ -1,25 +1,73 @@
-// src/app/services/token.service.ts
 import { Injectable } from '@angular/core';
+import {jwtDecode} from 'jwt-decode';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TokenService {
-  private tokenKey = 'auth-token';
+  private apiUrl = 'http://localhost:8085/api/auth/validate'; // Cambia esta URL a la de tu backend
 
+  constructor(private http: HttpClient) {}
+
+  // Verifica si el token ha expirado
+  isTokenExpired(token: string): boolean {
+    try {
+      const decodedToken: any = jwtDecode(token);
+      if (!decodedToken || !decodedToken.exp) {
+        return true;
+      }
+
+      // Verificación por la fecha de expiración almacenada
+      const tokenData = localStorage.getItem('token');
+      if (tokenData) {
+        const parsedToken = JSON.parse(tokenData);
+        if (new Date().getTime() > parsedToken.expiresAt) {
+          return true;
+        }
+      }
+
+      const expirationTime = decodedToken.exp * 1000;
+      return new Date().getTime() > expirationTime;
+    } catch (e) {
+      return true;
+    }
+  }
+
+
+  // Verifica si el token ha expirado llamando al backend
+  validateTokenBackend(token: string): Observable<boolean> {
+    return this.http.post<boolean>(this.apiUrl, { token });
+  }
+
+  // Guarda el token en localStorage
   setToken(token: string): void {
-    console.log('Guardando token en localStorage:', token); // Verifica que el token esté llegando aquí
-    localStorage.setItem('token', token);
+    const expirationTime = new Date().getTime() + 1000 * 60 * 60 * 24; // 24 horas desde ahora
+    let tokenData = {
+      value: token,
+      expiresAt: expirationTime
+    };
+    localStorage.setItem('token', JSON.stringify(tokenData));
   }
 
+  // Obtiene el token del localStorage
   getToken(): string | null {
-    const token = localStorage.getItem('token');
-    console.log('Obteniendo token de localStorage:', token); // Verifica que el token se obtiene correctamente
-    return token;
+    let tokenData = localStorage.getItem('token');
+    if (tokenData) {
+      let parsedToken = JSON.parse(tokenData);
+      if (new Date().getTime() > parsedToken.expiresAt) {
+        this.removeToken();
+        return null;
+      }
+      return parsedToken.value;
+    }
+    return null;
   }
 
 
-  clearToken(): void {
-    localStorage.removeItem(this.tokenKey);
+  // Elimina el token del localStorage
+  removeToken(): void {
+    localStorage.removeItem('token');
   }
 }
